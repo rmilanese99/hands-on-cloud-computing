1c8f25cbad0d07eefb3c4ae02a7488ed20115528
Roberto Milanese <r.milanese1@studenti.unimol.it>
2023-08-30 11:09:30 +0200

    Define ASG for EC2 instances

--- iac/bin/app.ts
+++ iac/bin/app.ts
@@ -1,7 +1,6 @@
 import { App, Stack } from "aws-cdk-lib";
-import { Peer, Port, SecurityGroup, SubnetType, Vpc } from "aws-cdk-lib/aws-ec2";
-//TODO: DAgg-New
-import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import { AutoScalingGroup } from "aws-cdk-lib/aws-autoscaling";
+import { InstanceClass, InstanceSize, InstanceType, LaunchTemplate, MachineImage, Peer, Port, SecurityGroup, SubnetType, Vpc } from "aws-cdk-lib/aws-ec2";
 
 export class FloraStack extends Stack {
 
@@ -11,7 +10,6 @@ export class FloraStack extends Stack {
         super(app, id);
 
         // Create a VPC spanning 2 AZs, with a public subnet in each
-        // Create VPC in which we'll launch the Instance
         const vpc = new Vpc(this, `${FloraStack.PREFIX}-vpc`, {
             maxAzs: 2,
             natGateways: 0,
@@ -32,23 +30,27 @@ export class FloraStack extends Stack {
         ec2_sg.addIngressRule(Peer.anyIpv4(), Port.tcp(22), 'Allow SSH access from anywhere');
         ec2_sg.addIngressRule(alb_sg, Port.tcp(80), 'Allow HTTP traffic from the ALB');
 
-        //TODO: DAgg-New
-        const ec2Instace = new ec2.Instance(this, `${FloraStack.PREFIX}-ec2-instance`, {
-            vpc:vpc,
-            vpcSubnets:{
-                subnetType: ec2.SubnetType.PUBLIC 
+        // Define launch template for the EC2 instances in the ASG
+        const ec2_template = new LaunchTemplate(this, `${FloraStack.PREFIX}-ec2-template`, {
+            instanceType: InstanceType.of(
+                InstanceClass.C7G,
+                InstanceSize.MEDIUM),
+            machineImage: MachineImage.latestAmazonLinux2023()
+        });
+
+        // Create an ASG with 2-4 EC2 instances distributed across the 2 AZs
+        const ec2_asg = new AutoScalingGroup(this, `${FloraStack.PREFIX}-ec2-asg`, {
+            launchTemplate: ec2_template,
+            vpc,
+            vpcSubnets: {
+                // Selects all public subnets across all AZs
+                subnetType: SubnetType.PUBLIC
             },
             securityGroup: ec2_sg,
-            //Ovviamente la classe è da valutare bene
-            instanceType: ec2.InstanceType.of(
-                ec2.InstanceClass.C7G,
-                ec2.InstanceSize.MEDIUM),
-            machineImage: new ec2.AmazonLinuxImage({
-                generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2
-            }),
-            //the name of the SSH key pair we are going to use to SSH into the instance
-            keyName: 'ec2-flora-unimol',
-
+            // Auto Scaling tries to maintain equivalent numbers of instances in each AZ
+            desiredCapacity: 2,
+            minCapacity: 2,
+            maxCapacity: 4
         });
     }
 
