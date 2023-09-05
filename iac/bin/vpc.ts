616ae5d9f06128113c9c2392a310c9ead99c16d0
Roberto Milanese <r.milanese1@studenti.unimol.it>
2023-09-05 17:57:38 +0200

    Remove SSH ingress rule on EC2 security group

--- iac/bin/vpc.ts
+++ iac/bin/vpc.ts
@@ -37,7 +37,6 @@ export class VpcResources extends Construct {
         alb_sg.addIngressRule(vpc_link_sg, Port.tcp(80), 'Allow HTTP traffic from the VPC Link');
 
         // Define ingress rules for the EC2 security group
-        ec2_sg.addIngressRule(Peer.anyIpv4(), Port.tcp(22), 'Allow SSH access from anywhere');
         ec2_sg.addIngressRule(alb_sg, Port.tcp(8080), 'Allow HTTP traffic from the ALB');
 
         // Define ingress rules for the EFS security group
