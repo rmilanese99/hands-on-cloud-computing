fa140b6a32f93221cccde0685f334270bf95bc52
Roberto Milanese <r.milanese1@studenti.unimol.it>
2023-09-05 17:57:16 +0200

    Change server port for EC2 security group

--- iac/bin/vpc.ts
+++ iac/bin/vpc.ts
@@ -38,7 +38,7 @@ export class VpcResources extends Construct {
 
         // Define ingress rules for the EC2 security group
         ec2_sg.addIngressRule(Peer.anyIpv4(), Port.tcp(22), 'Allow SSH access from anywhere');
-        ec2_sg.addIngressRule(alb_sg, Port.tcp(80), 'Allow HTTP traffic from the ALB');
+        ec2_sg.addIngressRule(alb_sg, Port.tcp(8080), 'Allow HTTP traffic from the ALB');
 
         // Define ingress rules for the EFS security group
         efs_sg.addIngressRule(ec2_sg, Port.tcp(2049), 'Allow NFS traffic from the EC2 instances');
